13ca58747bdbe11b1e27a6d74f7689cd0db1f3c8
yuniquintero <yuni.quintero@gmail.com>
2023-04-05 14:48:31 +0200

    Initial Commit

---
+++ RandomQuote/RandomQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteApp.swift
+//  RandomQuote
+//
+//  Created by Yuni Quintero on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
